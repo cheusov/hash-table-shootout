c60a589b35d16515be9c0fd3e5710f92220775e3
Aleksey Cheusov <vle@gmx.net>
2022-06-03 23:51:08 +0400

    Fixes for recent changes in nata88

--- src/nata88.cc
+++ src/nata88.cc
@@ -11,24 +11,24 @@
 #define DELETE_INT(key) \
 	nata88del(&hash_int, key, NULL);
 #define FIND_INT_EXISTING(key) \
-	if (nata88get(hash_int, key, NULL) == NULL) { \
+	if (nata88get(hash_int, key) == NULL) { \
 		std::cerr << "error\n"; \
 		exit(1); \
 	}
 #define FIND_INT_MISSING(key) \
-	if (nata88get(hash_int, key, NULL) != NULL) { \
+	if (nata88get(hash_int, key) != NULL) { \
 		std::cerr << "error\n"; \
 		exit(1); \
 	}
 #define FIND_INT_EXISTING_COUNT(key, count) \
-	if (nata88get(hash_int, key, NULL) != NULL) { \
+	if (nata88get(hash_int, key) != NULL) { \
 		count++; \
 	}
 #define CHECK_INT_ITERATOR_VALUE(iterator, value)
 #define ITERATE_INT(key) \
 	uint64_t key = 0; \
-	uint64_t *p_value = nata88first(hash_int, &key, NULL); \
-	for (; p_value != NULL; p_value = nata88next(hash_int, &key, NULL))
+	uint64_t *p_value = nata88first(hash_int, &key); \
+	for (; p_value != NULL; p_value = nata88next(hash_int, &key))
 #define LOAD_FACTOR_INT_HASH(hash) (0.0f)
 #define CLEAR_INT
 
