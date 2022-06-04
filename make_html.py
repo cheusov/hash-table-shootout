31d0780c3aef91ed9c4d3e0db5d7ef834515cc80
Aleksey Cheusov <vle@gmx.net>
2022-06-04 22:48:12 +0400

    make_html.py: remove items from "Jump to" lists when appropriate items are not available

--- make_html.py
+++ make_html.py
@@ -7,45 +7,50 @@ html_template = open('charts-template.html', 'r').read()
 charts_data = sys.stdin.read()
 
 repls = (
-    ('insert_random_shuffle_range',    'INSERT_RANDOM_SHUFFLE_RANGE'),
-    ('reinsert_random_shuffle_range',  'REINSERT_RND_SHUFFLE_RANGE'),
+    'insert_random_shuffle_range',
+    'reinsert_random_shuffle_range',
 
-    ('read_random_shuffle_range',      'READ_RANDOM_SHUFFLE_RANGE'),
-    ('insert_random_full',             'INSERT_RANDOM_FULL'),
-    ('reinsert_random_full',           'REINSERT_RND_FULL'),
-    ('insert_random_full_reserve',     'INSERT_RANDOM_F_RESERVE'),
-    ('read_random_full',               'READ_RANDOM_FULL'),
-    ('read_miss_random_full',          'READ_MISS_RANDOM_FULL'),
-    ('delete_random_full',             'DELETE_RANDOM_FULL'),
-    ('read_random_full_after_delete',  'READ_RANDOM_F_AFTER_DELETE'),
-    ('iteration_random_full',          'ITERATION_RANDOM_FULL'),
+    'read_random_shuffle_range',
+    'insert_random_full',
+    'reinsert_random_full',
+    'insert_random_full_reserve',
+    'read_random_full',
+    'read_miss_random_full',
+    'delete_random_full',
+    'read_random_full_after_delete',
+    'iteration_random_full',
 
-    ('insert_small_string',            'INSERT_SMALL_STRING'),
-    ('reinsert_small_string',          'REINSERT__SMALL__STRING'),
-    ('insert_small_string_reserve',    'RESERVE_SMALL_STRING'),
-    ('read_small_string',              'READ_SMALL_STRING'),
-    ('read_miss_small_string',         'READ_MISS_SMALL_STRING'),
-    ('delete_small_string',            'READ_AFTER_DEL_SMALL_STRING'),
-    ('read_small_string_after_delete', 'DELETE_SMALL_STRING'),
+    'insert_small_string',
+    'reinsert_small_string',
+    'insert_small_string_reserve',
+    'read_small_string',
+    'read_miss_small_string',
+    'delete_small_string',
+    'read_small_string_after_delete',
 
-    ('insert_string',                  'INSERT_STRING'),
-    ('reinsert_string',                'REINSERT__STRING'),
-    ('insert_string_reserve',          'RESERVE_STRING'),
-    ('read_string',                    'READ_STRING'),
-    ('read_miss_string',               'READ_MISS_STRING'),
-    ('delete_string',                  'READ_AFTER_DEL_STRING'),
-    ('read_string_after_delete',       'DELETE_STRING'),
+    'insert_string',
+    'reinsert_string',
+    'insert_string_reserve',
+    'read_string',
+    'read_miss_string',
+    'delete_string',
+    'read_string_after_delete',
 )
 
 for l in repls:
-    if charts_data.find(l[0]) < 0:
-        html_template = re.sub('BEGIN_' + l[1] + "(.|\\n)*END_" + l[1], '',
+    if charts_data.find(l) < 0:
+        html_template = re.sub('BEGIN2_' + l.upper() + " (.|\\n)*END2_" + l.upper() + " ", '',
+                               html_template)
+        html_template = re.sub('BEGIN_' + l.upper() + " (.|\\n)*END_" + l.upper() + " ", '',
                                html_template)
 
 html_template = re.sub('<!-- *-->', '', html_template)
 
-for s in ['INTEGERS', 'SMALL_STRINGS', 'STRINGS']:
-    html_template = re.sub('REAL_BEGIN_' + s + '(.|\\n)*BEGIN_' + s
-                           + ' -->(\\s|\\n)*<!-- END_' + s, '', html_template)
+for s in ['INTEGERS_RANGE', 'INTEGERS_RANDOM_FULL', 'SMALL_STRINGS', 'STRINGS', 'INTEGERS']:
+    html_template = re.sub('<!-- REAL_BEGIN_' + s + '(.|\\n)*BEGIN_' + s
+                           + ' -->(\\s|\\n)*<!-- END_' + s + " -->", '', html_template)
+    html_template = re.sub('<!-- REAL2_BEGIN_' + s + '.*BEGIN2_' + s
+                           + ' -->(\\s|\\n)*</ul>(\\s|\\n)*</li>(\\s|\\n)*<!-- END2_' + s + " -->", '',
+                           html_template, 0, re.DOTALL)
 
 open('charts.html', 'w').write(html_template.replace('__CHART_DATA_GOES_HERE__', charts_data))
