9d849f97d8b80d9a010c99af6dfee6d509dc8f4e
Aleksey Cheusov <vle@gmx.net>
2024-04-29 13:23:32 +0400

    Minor clean-ups in src/tsl_robin_map_store_hash.cc

--- src/tsl_robin_map_store_hash.cc
+++ src/tsl_robin_map_store_hash.cc
@@ -2,9 +2,9 @@
 #include <string>
 #include <tsl/robin_map.h>
 
-typedef tsl::robin_map<int64_t, int64_t, std::hash<int64_t>, std::equal_to<int64_t>, 
+typedef tsl::robin_map<int64_t, int64_t, std::hash<int64_t>, std::equal_to<int64_t>,
                        std::allocator<std::pair<int64_t, int64_t>>, true> hash_t;
-typedef tsl::robin_map<std::string, int64_t, std::hash<std::string>, std::equal_to<std::string>, 
+typedef tsl::robin_map<std::string, int64_t, std::hash<std::string>, std::equal_to<std::string>,
                        std::allocator<std::pair<std::string, int64_t>>, true> str_hash_t;
 
 #include "hash_map_int_base.h"
